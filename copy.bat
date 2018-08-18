78e6f117532982f4e200cd32d928c40b07d1ebf2
haoming37 <nh.ornis@gmail.com>
2018-08-18 16:10:05 +0900

    Create copy.bat

---
+++ copy.bat
@@ -0,0 +1,30 @@
+@echo off
+pause
+set basedir=%~dp0
+echo basedir = %basedir%
+if exist %UserProfile%/.android (
+    echo %UserProfile%/.android already exists
+    pause
+    exit
+)
+if exist %UserProfile%/.AndroidStudio3.1 (
+    echo %UserProfile%/.AndroidStudio3.1 already exists
+    pause
+    exit
+)
+if exist %UserProfile%/.gradle (
+    echo %UserProfile%/.gradle already exists
+    pause
+    exit
+)
+if exist %localappdata%/Android/Sdk (
+    echo %localappdata%/Android/Sdk already exists
+    pause
+    exit
+)
+robocopy %basedir%/.android %UserProfile%/.android /s /e
+robocopy %basedir%/.gradle %UserProfile%/.gradle /s /e
+robocopy %basedir%/.AndroidStudio3.1 %UserProfile%/AndroidStudio3.1 /s /e
+robocopy %basedir%/Sdk %localappdata%/Android/Sdk /s /e
+echo CopyComplete
+pause
